a7a2bff674dc1f0289b992f5d636ef383f25c28c
ItVergil251 <ivanaskad@gmail.com>
2024-03-23 13:43:52 +0000

    Mod+Add pins

--- Projects/VS_Code_PlatformIO/240323-121425-nanoatmega328/src/Drone_JoyStick.ino
+++ Projects/VS_Code_PlatformIO/240323-121425-nanoatmega328/src/Drone_JoyStick.ino
@@ -1,13 +1,21 @@
 #define AngleX A0  // Підключення піна для вимірювання кута X (Connection of the pin for measuring angle X)
 #define AngleY A1  // Підключення піна для вимірювання кута Y (Connection of the pin for measuring angle Y)
-// #define AngleReset 1
 
 #define DriveZ A2  // Підключення піна для управління осьовою швидкістю Z (Connection of the pin for controlling axial speed Z)
 #define DriveR A3  // Підключення піна для управління обертовою швидкістю R (Connection of the pin for controlling rotational speed R)
-// #define DriveReset 1
+
+#define Butt_A 2
+#define Butt_D 3
+
+#define ButtonS_ADC A4
+
+#define NRF_CS 4
+#define NRF_CE 5
 
 uint16_t
-  data_send[4] = {0};
+  data_send[6] = {0},
+  data_check[4] = {0};
+
 byte
   joystick_pins[4] = {DriveZ, DriveR, AngleY, AngleX};
 
@@ -16,13 +24,14 @@ void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(joystick_pins[i], INPUT);
   }
-  // pinMode(AngleReset, INPUT_PULLUP);
-  // pinMode(DriveReset, INPUT_PULLUP);
+  pinMode(Butt_A, INPUT_PULLUP);
+  pinMode(Butt_D, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   for (int i = 0; i < 4; i++) {
-    data_send[i] = analogRead(joystick_pins[i]);
+    data_check[i] = analogRead(joystick_pins[i]);
+    data_send [i] = (data_send[i] != data_check[i])?data_check[i]:data_send[i];
   }
 }
